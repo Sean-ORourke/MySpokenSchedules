6269216e69d480e3800f7d5a42a0fe44d5d3b3c4
Sean-ORourke <hellohinope@gmail.com>
2024-11-21 16:30:58 -0500

    starting to add schedule model

--- my_spoken_schedules/lib/model/task_model.dart
+++ my_spoken_schedules/lib/model/task_model.dart
@@ -1,24 +1,15 @@
 class task_model {
-
   String? id;
   String? label;
   String? time;
   String? message;
-  bool? isActive;
 
-  task_model (
-  {this.id,
-  this.label,
-  this.time,
-  this.message,
-  this.isActive});
+  task_model({this.id, this.label, this.time, this.message});
 
   task_model.fromJson(Map<String, dynamic> json) {
     id = json['id'];
     label = json['label'];
     time = json['time'];
     message = json['message'];
-    isActive = json['isActive'];
   }
-
 }
