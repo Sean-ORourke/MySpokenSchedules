a8d736ba6995d5464fe0e3310193f6b1a915392f
BroccoLi25 <erl88@pitt.edu>
2024-12-09 23:31:32 -0500

    text-to-speech in notification.dart
    build.gradle midSdkVersion 21
    main.dart package added for tts
    notification.dart implemented

--- my_spoken_schedules/macos/Flutter/GeneratedPluginRegistrant.swift
+++ my_spoken_schedules/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,8 +7,10 @@ import Foundation
 
 import flutter_local_notifications
 import flutter_timezone
+import flutter_tts
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   FlutterTimezonePlugin.register(with: registry.registrar(forPlugin: "FlutterTimezonePlugin"))
+  FlutterTtsPlugin.register(with: registry.registrar(forPlugin: "FlutterTtsPlugin"))
 }
